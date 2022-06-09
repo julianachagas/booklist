5dbb51d44e6838e436bc02fc8c1b1310f3d3220b
Juliana Chagas <julianavrchagas@gmail.com>
2022-06-09 17:59:21 -0300

    feat: add localStorage

--- app.js
+++ app.js
@@ -2,6 +2,44 @@ const bookList = document.getElementById('book-list')
 const emptyListInstruction = document.querySelector('#empty-list-instruction')
 const hideBooks = document.querySelector('#hide-books-wrapper')
 const inputValidation = document.querySelector('#input-validation')
+const addForm = document.forms['add-book']
+
+//get books from localStorage
+function getBooks() {
+  let books
+  //check if there is no item stored (null), create an empty array
+  if (!localStorage.getItem('books')) {
+    books = []
+  } else {
+    //get the string stored in localStorage and turn into an array
+    books = JSON.parse(localStorage.getItem('books'))
+  }
+  //returns an empty array or array of stored books
+  return books
+}
+
+function createBookListItem(bookObject) {
+  const listItem = document.createElement('li')
+  const bookName = document.createElement('span')
+  const deleteBtn = document.createElement('button')
+  bookName.classList.add('book-name')
+  deleteBtn.classList.add('btn', 'delete')
+  deleteBtn.textContent = 'delete'
+  bookName.textContent = bookObject.title
+  listItem.append(bookName, deleteBtn)
+  bookList.appendChild(listItem)
+}
+
+function displayBooks() {
+  //get the books stored
+  const storedBooks = getBooks()
+  if (storedBooks.length !== 0) {
+    //display the books stored
+    storedBooks.forEach(book => createBookListItem(book))
+    //remove initial instructions and show checkbox to hide the books
+    notEmptyBookListState()
+  }
+}
 
 function addShowClass(element) {
   element.classList.add('show')
@@ -11,57 +49,98 @@ function removeShowClass(element) {
   element.classList.remove('show')
 }
 
-function emptyBookListState() {
-  addShowClass(emptyListInstruction)
-  removeShowClass(hideBooks)
-}
-
 function notEmptyBookListState() {
   removeShowClass(emptyListInstruction)
   addShowClass(hideBooks)
 }
 
-//delete book
-bookList.addEventListener('click', function (e) {
-  if (e.target.tagName == 'BUTTON') {
+function emptyBookListState() {
+  addShowClass(emptyListInstruction)
+  removeShowClass(hideBooks)
+}
+
+//Event: display books when pages load
+document.addEventListener('DOMContentLoaded', displayBooks)
+
+class Book {
+  constructor(title) {
+    this.title = title
+  }
+}
+
+function addBookToStorage(bookObject) {
+  const storedBooks = getBooks()
+  storedBooks.push(bookObject)
+  setLocalStorage('books', storedBooks)
+}
+
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+function removeBookFromStorage(bookTitle) {
+  const storedBooks = getBooks()
+  storedBooks.forEach((item, index) => {
+    if (item.title === bookTitle) {
+      //remove it from the array
+      storedBooks.splice(index, 1)
+      //store new array to local storage
+      setLocalStorage('books', storedBooks)
+    }
+  })
+}
+
+function clearField() {
+  addForm.querySelector("input[type='text']").value = ''
+}
+
+function focusField() {
+  addForm.querySelector("input[type='text']").focus()
+}
+
+function addBook() {
+  const title = addForm.querySelector('#add-book-title').value
+  //validate
+  if (title !== '') {
+    //create book object, instatiate book
+    const newBook = new Book(title)
+    //Add book to UI
+    createBookListItem(newBook)
+    //Add book to store
+    addBookToStorage(newBook)
+    removeShowClass(inputValidation)
+    notEmptyBookListState()
+    clearField()
+  } else {
+    addShowClass(inputValidation)
+    focusField()
+  }
+}
+
+function deleteBook(e) {
+  if (e.target.classList.contains("delete")) {
     const listItem = e.target.parentElement
-    bookList.removeChild(listItem)
-    removeShowClass(inputValidation)    
+    const bookTitle = listItem.querySelector('.book-name').textContent
+    //remove from UI
+    listItem.remove()
+    //bookList.removeChild(listItem)
+    //remove from storage
+    removeBookFromStorage(bookTitle)
+    removeShowClass(inputValidation)
     if (bookList.children.length === 0) {
       emptyBookListState()
     }
   }
-})
+}
 
-//add book
-
-const addForm = document.forms['add-book']
-
+//Event: add a book
 addForm.addEventListener('submit', function (e) {
   e.preventDefault()
-  const newBook = addForm.querySelector("input[type='text']").value
-  if (newBook !== '') {
-    bookList.appendChild(createBookListItem(newBook))
-    notEmptyBookListState()
-    removeShowClass(inputValidation)    
-    addForm.querySelector("input[type='text']").value = ''
-  } else {
-    addShowClass(inputValidation)    
-    addForm.querySelector("input[type='text']").focus()
-  }
+  addBook()
 })
 
-function createBookListItem(book) {
-  const listItem = document.createElement('li')
-  const bookName = document.createElement('span')
-  const deleteBtn = document.createElement('button')
-  bookName.classList.add('book-name')
-  deleteBtn.classList.add('btn', 'delete')
-  deleteBtn.textContent = 'delete'
-  bookName.textContent = book
-  listItem.append(bookName, deleteBtn)
-  return listItem
-}
+//Event: delete book
+bookList.addEventListener('click', deleteBook)
 
 //hide books
 const hideCheckBox = document.getElementById('hide-books')
@@ -69,26 +148,16 @@ hideCheckBox.addEventListener('change', function () {
   bookList.classList.toggle('hide', hideCheckBox.checked)
 })
 
-//filter books
-const searchBar = document.getElementById('search-book-title')
-searchBar.addEventListener('keyup', function (e) {
-  const term = e.target.value.toLowerCase()
-  const books = bookList.querySelectorAll('li')
-  books.forEach(book => {
-    const bookTitle = book.firstElementChild.textContent.toLowerCase()
-    book.classList.toggle('hide', !bookTitle.includes(term))
-  })
-})
-
 //tabs
-
 const tabs = document.querySelector('.tabs') //ul
 const panels = document.querySelectorAll('.panel') //panel divs
-const liItems = tabs.querySelectorAll('li')
+const tabsItems = tabs.querySelectorAll('li')
 
-tabs.addEventListener('click', function (e) {
+tabs.addEventListener('click', showPanel)
+
+function showPanel(e) {
   if (e.target.tagName == 'LI') {
-    liItems.forEach(item => {
+    tabsItems.forEach(item => {
       item.classList.toggle('active', e.target == item)
     })
     const targetPanel = document.querySelector(e.target.dataset.target)
@@ -96,15 +165,28 @@ tabs.addEventListener('click', function (e) {
       panel.classList.toggle('active', panel == targetPanel)
     })
   }
-  removeShowClass(inputValidation)  
-})
+  removeShowClass(inputValidation)
+}
 
 function hidePanel() {
   panels.forEach(panel => {
     panel.classList.remove('active')
   })
-  liItems.forEach(item => {
+  tabsItems.forEach(item => {
     item.classList.remove('active')
+  })
+}
+
+//filter books
+const searchBar = document.getElementById('search-book-title')
+searchBar.addEventListener('keyup', filterBooks)
+
+function filterBooks(e) {
+  const term = e.target.value.toLowerCase()
+  const books = bookList.querySelectorAll('li')
+  books.forEach(book => {
+    const bookTitle = book.firstElementChild.textContent.toLowerCase()
+    book.classList.toggle('hide', !bookTitle.includes(term))
   })
 }
 
