bbb569285eba39e4c62e77e3a07dbc974267a3d1
Juliana Chagas <julianavrchagas@gmail.com>
2022-06-10 22:27:55 -0300

    feat: add classes

--- app.js
+++ app.js
@@ -1,146 +1,195 @@
-const bookList = document.getElementById('book-list')
-const emptyListInstruction = document.querySelector('#empty-list-instruction')
-const hideBooks = document.querySelector('#hide-books-wrapper')
-const inputValidation = document.querySelector('#input-validation')
-const addForm = document.forms['add-book']
-
-//get books from localStorage
-function getBooks() {
-  let books
-  //check if there is no item stored (null), create an empty array
-  if (!localStorage.getItem('books')) {
-    books = []
-  } else {
-    //get the string stored in localStorage and turn into an array
-    books = JSON.parse(localStorage.getItem('books'))
-  }
-  //returns an empty array or array of stored books
-  return books
-}
-
-function createBookListItem(bookObject) {
-  const listItem = document.createElement('li')
-  const bookName = document.createElement('span')
-  const deleteBtn = document.createElement('button')
-  bookName.classList.add('book-name')
-  deleteBtn.classList.add('btn', 'delete')
-  deleteBtn.textContent = 'delete'
-  bookName.textContent = bookObject.title
-  listItem.append(bookName, deleteBtn)
-  bookList.appendChild(listItem)
-}
-
-function displayBooks() {
-  //get the books stored
-  const storedBooks = getBooks()
-  if (storedBooks.length !== 0) {
-    //display the books stored
-    storedBooks.forEach(book => createBookListItem(book))
-    //remove initial instructions and show checkbox to hide the books
-    notEmptyBookListState()
-  }
-}
-
-function addShowClass(element) {
-  element.classList.add('show')
-}
-
-function removeShowClass(element) {
-  element.classList.remove('show')
-}
-
-function notEmptyBookListState() {
-  removeShowClass(emptyListInstruction)
-  addShowClass(hideBooks)
-}
-
-function emptyBookListState() {
-  addShowClass(emptyListInstruction)
-  removeShowClass(hideBooks)
-}
-
-//Event: display books when pages load
-document.addEventListener('DOMContentLoaded', displayBooks)
-
 class Book {
   constructor(title) {
     this.title = title
   }
 }
 
-function addBookToStorage(bookObject) {
-  const storedBooks = getBooks()
-  storedBooks.push(bookObject)
-  setLocalStorage('books', storedBooks)
-}
-
-function setLocalStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value))
-}
-
-function removeBookFromStorage(bookTitle) {
-  const storedBooks = getBooks()
-  storedBooks.forEach((item, index) => {
-    if (item.title === bookTitle) {
-      //remove it from the array
-      storedBooks.splice(index, 1)
-      //store new array to local storage
-      setLocalStorage('books', storedBooks)
+class Store {
+  //get books from localStorage
+  static getBooks() {
+    let books
+    //check if there is no item stored (null), create an empty array
+    if (!localStorage.getItem('books')) {
+      books = []
+    } else {
+      //get the string stored in localStorage and turn into an array
+      books = JSON.parse(localStorage.getItem('books'))
     }
-  })
-}
+    //returns an empty array or array of stored books
+    return books
+  }
 
-function clearField() {
-  addForm.querySelector("input[type='text']").value = ''
-}
+  static setLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
 
-function focusField() {
-  addForm.querySelector("input[type='text']").focus()
-}
+  static addBookToStorage(bookObject) {
+    const storedBooks = Store.getBooks()
+    storedBooks.push(bookObject)
+    Store.setLocalStorage('books', storedBooks)
+  }
 
-function addBook() {
-  const title = addForm.querySelector('#add-book-title').value
-  //validate
-  if (title !== '') {
-    //create book object, instatiate book
-    const newBook = new Book(title)
-    //Add book to UI
-    createBookListItem(newBook)
-    //Add book to store
-    addBookToStorage(newBook)
-    removeShowClass(inputValidation)
-    notEmptyBookListState()
-    clearField()
-  } else {
-    addShowClass(inputValidation)
-    focusField()
+  static removeBookFromStorage(bookTitle) {
+    const storedBooks = Store.getBooks()
+    storedBooks.forEach((item, index) => {
+      if (item.title === bookTitle) {
+        //remove it from the array
+        storedBooks.splice(index, 1)
+      }
+    })
+    //store new array to local storage
+    Store.setLocalStorage('books', storedBooks)
   }
 }
 
-function deleteBook(e) {
-  if (e.target.classList.contains("delete")) {
-    const listItem = e.target.parentElement
-    const bookTitle = listItem.querySelector('.book-name').textContent
-    //remove from UI
-    listItem.remove()
-    //bookList.removeChild(listItem)
-    //remove from storage
-    removeBookFromStorage(bookTitle)
-    removeShowClass(inputValidation)
-    if (bookList.children.length === 0) {
-      emptyBookListState()
+class UI {
+  static createBookListItem(bookObject) {
+    const bookList = document.getElementById('book-list')
+    const listItem = document.createElement('li')
+    const bookName = document.createElement('span')
+    const deleteBtn = document.createElement('button')
+    bookName.classList.add('book-name')
+    deleteBtn.classList.add('btn', 'delete')
+    deleteBtn.textContent = 'delete'
+    bookName.textContent = bookObject.title
+    listItem.append(bookName, deleteBtn)    
+    bookList.appendChild(listItem)
+  }
+
+  static notEmptyBookListState() {
+    const emptyListInstruction = document.querySelector(
+      '#empty-list-instruction'
+    )
+    const hideBooks = document.querySelector('#hide-books-wrapper')
+    emptyListInstruction.classList.remove('show')
+    hideBooks.classList.add('show')
+  }
+
+  static emptyBookListState() {
+    const emptyListInstruction = document.querySelector(
+      '#empty-list-instruction'
+    )
+    const hideBooks = document.querySelector('#hide-books-wrapper') 
+    emptyListInstruction.classList.add('show')
+    hideBooks.classList.remove('show')
+  }
+ 
+  static displayBooks() {
+    //get the books stored
+    const storedBooks = Store.getBooks()    
+    if (storedBooks.length !== 0) {
+      //display the books stored
+      storedBooks.forEach(function(book) {
+        UI.createBookListItem(book)
+      }) 
+      //remove initial instructions and show checkbox to hide the books
+      UI.notEmptyBookListState() 
     }
   }
+
+  static clearField() {
+    document.forms['add-book'].querySelector('#add-book-title').value = ''
+  }
+
+  static focusField() {
+    document.forms['add-book'].querySelector('#add-book-title').focus()
+  }
+
+  static filterBooks(e) {
+    const term = e.target.value.toLowerCase()
+    const books = bookList.querySelectorAll('li')
+    books.forEach(book => {
+      const bookTitle = book.firstElementChild.textContent.toLowerCase()
+      book.classList.toggle('hide', !bookTitle.includes(term))
+    })
+  }
+
+  static addBook() {
+    const title = document.querySelector('#add-book-title').value
+    // validate
+    if (title !== '') {
+      //create book object, instatiate book
+      const newBook = new Book(title)
+      //Add book to UI
+      UI.createBookListItem(newBook)
+      //Add book to store
+      Store.addBookToStorage(newBook)
+      UI.notEmptyBookListState()
+      UI.clearField()
+    } else {
+      UI.showInputValidation()
+      UI.focusField()
+    }
+  }
+
+  static showInputValidation() {
+    const inputValidation = document.querySelector('#input-validation')
+    inputValidation.classList.add('show')
+    setTimeout(() => {
+      inputValidation.classList.remove('show')
+    }, 3000)
+  }
+
+  static deleteBook(e) {
+    const bookList = document.getElementById('book-list')
+    if (e.target.classList.contains('delete')) {
+      const listItem = e.target.parentElement
+      const bookTitle = listItem.querySelector('.book-name').textContent
+      //remove from UI
+      listItem.remove()
+      //remove from storage
+      Store.removeBookFromStorage(bookTitle)
+      //UI.removeShowClass(inputValidation)
+      if (bookList.children.length === 0) {
+        UI.emptyBookListState()
+      }
+    }
+  }
+
+  static showPanel(e) {
+    const tabs = document.querySelector('.tabs')
+    const panels = document.querySelectorAll('.panel')
+    const tabsItems = tabs.querySelectorAll('li') 
+    if (e.target.tagName == 'LI') {      
+      tabsItems.forEach(item => {
+        item.classList.toggle('active', e.target == item)
+      })
+      const targetPanel = document.querySelector(e.target.dataset.target)
+      panels.forEach(panel => {
+        panel.classList.toggle('active', panel == targetPanel)
+      })
+    }
+  }
+
+  static hidePanel() {
+    const tabs = document.querySelector('.tabs')
+    const panels = document.querySelectorAll('.panel')
+    const tabsItems = tabs.querySelectorAll('li') 
+    panels.forEach(panel => {
+      panel.classList.remove('active')
+    })
+    tabsItems.forEach(item => {
+      item.classList.remove('active')
+    })
+  } 
 }
 
-//Event: add a book
+//display books when pages load
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+//add a book
+const addForm = document.forms['add-book']
 addForm.addEventListener('submit', function (e) {
   e.preventDefault()
-  addBook()
+  UI.addBook()
 })
 
-//Event: delete book
-bookList.addEventListener('click', deleteBook)
+//hide panel when user clicks on the form
+addForm.addEventListener('click', UI.hidePanel)
+
+//delete book
+const bookList = document.getElementById('book-list')
+bookList.addEventListener('click', UI.deleteBook)
 
 //hide books
 const hideCheckBox = document.getElementById('hide-books')
@@ -148,52 +197,12 @@ hideCheckBox.addEventListener('change', function () {
   bookList.classList.toggle('hide', hideCheckBox.checked)
 })
 
-//tabs
-const tabs = document.querySelector('.tabs') //ul
-const panels = document.querySelectorAll('.panel') //panel divs
-const tabsItems = tabs.querySelectorAll('li')
-
-tabs.addEventListener('click', showPanel)
-
-function showPanel(e) {
-  if (e.target.tagName == 'LI') {
-    tabsItems.forEach(item => {
-      item.classList.toggle('active', e.target == item)
-    })
-    const targetPanel = document.querySelector(e.target.dataset.target)
-    panels.forEach(panel => {
-      panel.classList.toggle('active', panel == targetPanel)
-    })
-  }
-  removeShowClass(inputValidation)
-}
-
-function hidePanel() {
-  panels.forEach(panel => {
-    panel.classList.remove('active')
-  })
-  tabsItems.forEach(item => {
-    item.classList.remove('active')
-  })
-}
-
 //filter books
 const searchBar = document.getElementById('search-book-title')
-searchBar.addEventListener('keyup', filterBooks)
+searchBar.addEventListener('keyup', UI.filterBooks)
+searchBar.addEventListener('click', UI.hidePanel)
 
-function filterBooks(e) {
-  const term = e.target.value.toLowerCase()
-  const books = bookList.querySelectorAll('li')
-  books.forEach(book => {
-    const bookTitle = book.firstElementChild.textContent.toLowerCase()
-    book.classList.toggle('hide', !bookTitle.includes(term))
-  })
-}
+//tabs
+const tabs = document.querySelector('.tabs') 
+tabs.addEventListener('click', UI.showPanel)
 
-//hide panel when user clicks on the add-books form or search input
-addForm.addEventListener('click', hidePanel)
-searchBar.addEventListener('click', hidePanel)
-//hide input-validation when user clicks on the search input
-searchBar.addEventListener('click', function () {
-  removeShowClass(inputValidation)
-})
